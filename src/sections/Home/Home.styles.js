bb050205718d248a348bfbccb92fa07d891a238c
Francisco González <devfranciscogonzalez@gmail.com>
2024-07-27 16:08:28 -0400

    add animation

--- src/sections/Home/Home.styles.js
+++ src/sections/Home/Home.styles.js
@@ -1,11 +1,31 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const slideUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(20px);
+    filter: blur(4px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+    filter: blur(0);
+  }
+`;
 
 const HomeUserWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   gap: ${({ theme }) => theme.spacing.small};
-  
+
+  & > :first-child {
+    animation: ${slideUp} 0.8s ease;
+  }
+  & > :last-child {
+    animation: ${slideUp} 0.8s ease;
+  }
+
   @media (width <= ${({ theme }) => theme.breakpoints.smallTablet}) {
     justify-content: center;
     align-items: center;
@@ -17,6 +37,7 @@ const HomeButtonWrapper = styled.div`
   display: flex;
   gap: ${({ theme }) => theme.spacing.medium};
   margin-top: ${({ theme }) => theme.spacing.xlarge};
+  animation: ${slideUp} 0.8s ease;
 
   @media (width <= ${({ theme }) => theme.breakpoints.smallTablet}) {
     gap: ${({ theme }) => theme.spacing.small};
