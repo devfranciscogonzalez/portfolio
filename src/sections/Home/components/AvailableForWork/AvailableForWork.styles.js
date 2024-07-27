6f9b94a2bd54140c13ebd70dc52b754be10c1173
Francisco González <devfranciscogonzalez@gmail.com>
2024-07-27 12:07:38 -0400

    fix dot

--- src/sections/Home/components/AvailableForWork/AvailableForWork.styles.js
+++ src/sections/Home/components/AvailableForWork/AvailableForWork.styles.js
@@ -1,7 +1,7 @@
 import styled, { keyframes } from "styled-components";
 
 const pulse = keyframes`
-  0% {
+  /* 0% {
     box-shadow: 0 0 0 0 rgb(76, 175, 80, 0.7);
   }
   
@@ -11,20 +11,19 @@ const pulse = keyframes`
   
   100% {
     box-shadow: 0 0 0 0 rgb(51, 217, 178, 0);
-  }
+  } */
 `;
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
   gap: ${({ theme }) => theme.spacing.small};
 `;
 
 const PulsingDot = styled.div`
-  display: inline-block;
-  width: 10px;
-  height: 10px;
+  width: 16px;
+  height: 16px;
+
   border-radius: 50%;
   animation: ${pulse} 2s infinite;
   background-color: rgb(76, 175, 80);
@@ -36,9 +35,12 @@ const PulsingDot = styled.div`
 `;
 
 const Text = styled.span`
+  height: 16px;
   font-family: ${({ theme }) => theme.fonts.primary};
-  font-size: ${({ theme }) => theme.fontSizes.medium};
-  color: ${({ theme }) => theme.colors.text.tertiary};
+  font-size: ${({ theme }) => theme.fontSizes.small};
+  text-transform: uppercase;
+
+  color: rgb(76, 175, 80);
 
   @media (width <= ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: ${({ theme }) => theme.fontSizes.small};
