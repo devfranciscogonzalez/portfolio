2fb9d634011d4eec9be618497c85ca3518eaa33a
Francisco González <devfranciscogonzalez@gmail.com>
2024-10-01 21:15:42 -0300

    Remove Instagram link from social media component and update curriculum link

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -1,11 +1,10 @@
-import { GitHub, Instagram, LinkedIn } from "../../assets/icons/socialMedia";
+import { GitHub, LinkedIn } from "../../assets/icons/socialMedia";
 import { SOCIAL_LINKS } from "../../constants/socialLinks";
 import { SocialMediaButton } from "./SocialMedia.styles";
 
 const socialMediaLinks = [
   { href: SOCIAL_LINKS.github, svg: GitHub, label: "GitHub" },
   { href: SOCIAL_LINKS.linkedin, svg: LinkedIn, label: "LinkedIn" },
-  { href: SOCIAL_LINKS.instagram, svg: Instagram, label: "Instagram" },
 ];
 
 const SocialMedia = () => (
--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,10 +1,9 @@
 const SOCIAL_LINKS = {
   linkedin: "https://linkedin.com/in/devfranciscog",
-  instagram: "https://www.instagram.com/fanchi.gp",
   github: "https://github.com/devfranciscogonzalez",
   email: "mailto:devfranciscogonzalez@gmail.com",
   curriculum:
-    "https://drive.google.com/file/d/10sRjAOK-X8xNjjVQeudhxOlJK2HeI37M/view?usp=sharing",
+    "https://drive.google.com/file/d/1ByrmKLeubE3BbWKR_sVAc6hrB7FL_GZj/view?usp=sharing",
 };
 
 export { SOCIAL_LINKS };
